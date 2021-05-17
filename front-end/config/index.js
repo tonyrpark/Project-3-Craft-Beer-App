8e35dad3c05a51dfa5b2b79a94f90eeceeabf198
tonyrpark <tonyrpark@gmail.com>
2021-05-16 20:31:16 -0700

    added FB login credentials, added code for server > index.js, stable stopping point for the night, tomorrow the plan is to continue adding the code to complete the fb login feature

--- front-end/config/index.js
+++ front-end/config/index.js
@@ -9,9 +9,14 @@ const GITHUB = {
   clientID: "ca07c74962097b8480f6",
   clientSecret: "a9428e7a75c59e24a32c0f473da2dbd42d5fbb89",
 };
+
+/*  ALT FACEBOOK CREDENTIALS
+
+    clientID: "2503599789703821",
+    clientSecret: "645088b310270cf3abbda58e01f65e96"*/
 const FACEBOOK = {
-  clientID: "2503599789703821",
-  clientSecret: "645088b310270cf3abbda58e01f65e96",
+  clientID: "141320861357658",
+  clientSecret: "e4c894f79797ba5892e17855d68a9ef2",
 };
 const GOOGLE = {
   clientID:
--- front-end/server/index.js
+++ front-end/server/index.js
@@ -1,31 +1,44 @@
-// CODE BELOW DOES NOT WORK, IT IS FROM A PREVIOUS PROJECT AND INDEX.JS OR SERVER.JS WILL NEED TO BE ADJUSTED -TONY
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook");
+const keys = require("../config");
+const chalk = require("chalk");
+let user = {};
 
-// import express from "express";
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose";
-// import cors from "cors";
 
-// import postRoutes from "./routes/posts.js";
+passport.serializeUser( fn: (user, cb) => {
+  cb(null, user); 
+}); 
 
-// const app = express();
+passport.deserializeUser((user, cb) => {
+  cb(null, user); 
+}); 
 
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors());
+// Facebook Strategy
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.FACEBOOK.clientID,
+      clientSecret: keys.FACEBOOK.clientSecret,
+      callbackUrl: "/auth/facebook/callback",
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(chalk.blue(JSON.stringify(profile)));
+      user = { ...profile };
+      return cb(null, profile); 
+    }
+  )
+);
 
-// app.use("/posts", postRoutes);
+const app = express(); 
+app.use(cors()); 
+app.use(passport.initialize()); 
 
-// const CONNECTION_URL =
-//   "mongodb+srv://tonyrpark:PqKbXn9g5oKiIKIw@practice.jto9p.mongodb.net/cryptoDb";
-// const PORT = process.env.PORT || 5000;
-
-// mongoose
-//   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() =>
-//     app.listen(PORT, () =>
-//       console.log(`Server Running on Port: http://localhost:${PORT}`)
-//     )
-//   )
-//   .catch((error) => console.log(`${error} did not connect`));
-
-// mongoose.set("useFindAndModify", false);
+app.get("/auth/facebook", passport.authenticate( strategy: "facebook")); 
+app.get("/auth/facebook/callback",
+  passport.authenticate(("facebook"),
+    options:(req, res) => {
+      res.redirect("/profile"); 
+    }
+  ));
